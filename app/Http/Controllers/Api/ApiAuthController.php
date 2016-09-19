08f7645008313677ee89aff4a2c28742b8c77263
firulete <sw4ttt@gmail.com>
2016-09-19 15:01:35 -0400

    jwt 1

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use Validator;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 
@@ -30,5 +30,41 @@ class ApiAuthController extends Controller
 
         // all good so return the token
         return response()->json(compact('token'));
+    }
+
+    public function signup(Request $request)
+    {
+        $signupFields = [
+            'name', 'email', 'password'
+            ];
+        $signupFieldRules = [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+            ];  
+
+        $hasToReleaseToken = true;
+
+        $userData = $request->only($signupFields);
+
+        $validator = Validator::make($userData, $signupFieldRules);
+
+        if($validator->fails()) {
+            throw new ValidationHttpException($validator->errors()->all());
+        }
+
+        User::unguard();
+        $user = User::create($userData);
+        User::reguard();
+
+        if(!$user->id) {
+            return $this->response->error('could_not_create_user', 500);
+        }
+
+        if($hasToReleaseToken) {
+            return $this->login($request);
+        }
+        
+        return $this->response->created();
     }
 }
