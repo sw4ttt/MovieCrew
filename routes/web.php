377121c1233a9c9f5b49ad3a0eae029607f9792a
firulete <sw4ttt@gmail.com>
2016-09-19 15:59:36 -0400

    jwt 1

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ Route::get('/', function () {
 });
 
 Route::group(['middleware' => ['api'],'prefix' => 'api'], function () {
-    Route::post('register', 'Api\ApiAuthController@register');
-    Route::post('login', 'Api\ApiAuthController@login');
+    Route::get('register', 'Api\ApiAuthController@register');
+    Route::get('login', 'Api\ApiAuthController@login');
     Route::group(['middleware' => 'jwt-auth'], function () {
     	Route::post('get_user_details', 'Api\ApiAuthController@get_user_details');
     });
