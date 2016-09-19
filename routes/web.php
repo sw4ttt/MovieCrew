406f8899782b17ccb559271a82e0495a370c0a2f
firulete <sw4ttt@gmail.com>
2016-09-19 14:43:40 -0400

    jwt 1

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('api', 'Api/ApiAuthController@authenticate');
+Route::get('api', 'Api\ApiAuthController@authenticate');
