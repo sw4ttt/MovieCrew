dd721042f5c71e835a1c46d08f13dcc2d00bcb40
firulete <sw4ttt@gmail.com>
2016-09-28 10:38:28 -0400

    stuff

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -44,8 +44,8 @@ class TestController extends Controller
  
           $content = json_decode($apiRequest->getBody()->getContents());
 
-          //var_dump($content);
-          return $content->data.Title;
+          var_dump($content->data);
+          //return $content->data.Title;
  
       } catch (RequestException $re) {
           //For handling exception
