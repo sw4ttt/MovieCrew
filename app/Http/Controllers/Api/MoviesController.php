747f666e7f537f69de1f358633c8372863fc7c22
firulete <sw4ttt@gmail.com>
2016-10-04 16:06:52 -0400

    test api

--- app/Http/Controllers/Api/MoviesController.php
+++ app/Http/Controllers/Api/MoviesController.php
@@ -185,7 +185,7 @@ class MoviesController extends Controller
         $movie = null;
         $result = null;
         $client = new GuzzleHttpClient();
-        $promise = $client->requestAsync('GET', 'http://api.myapifilms.com/imdb/idIMDB?idIMDB='.$request->IMDBid.'&token=d76a94d4-dccc-4e2d-a488-26cac8c258ba&simplePlot=1');
+        $promise = $client->requestAsync('GET', 'http://api.myapifilms.com/imdb/idIMDB?idIMDB='.$imdbid.'&token=d76a94d4-dccc-4e2d-a488-26cac8c258ba&simplePlot=1');
 
         $promise->then(
             function (ResponseInterface $res) 
