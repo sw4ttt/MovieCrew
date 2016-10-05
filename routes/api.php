fec1d389b815a09492718b9a48ebecfa30b835e5
firulete <sw4ttt@gmail.com>
2016-10-05 16:01:44 -0400

    stuff host

--- routes/api.php
+++ routes/api.php
@@ -49,7 +49,7 @@ Route::group(['middleware' => ['api','cors']], function ()
 
     // MOVIES Routes
     Route::get('movies', 'Api\MoviesController@index');
-    Route::get('getmovie', 'Api\MoviesController@getMovie');    
+    Route::post('getmovie', 'Api\MoviesController@getMovie');    
     
     Route::post('addmoviecrew', 'Api\MoviesController@addMovieToCrew');
     Route::post('getcrewmovies', 'Api\MoviesController@showCrewMovies');
