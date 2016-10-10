d4f156a77b58481b4261be6d6535bdbac81cf011
firulete <sw4ttt@gmail.com>
2016-10-10 13:03:24 -0400

    stuff 10-10

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,14 +14,18 @@
                             <thead>
                                 <tr>
                                     <th>Id</th>
+                                    <th>Name</th>
                                     <th>Email</th>
+                                    <th>Role</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach ($users as $user)
                                     <tr>
                                         <td>{{ $user->id }}</td>
+                                        <td>{{ $user->name }}</td>
                                         <td>{{ $user->email }}</td>
+                                        <td>{{ $user->role }}</td>
                                     </tr>
                                 @endforeach
                             </tbody>
